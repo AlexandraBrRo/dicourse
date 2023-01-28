47bba146105ae0cdfb6503ac32b417a379f7bce2
AlexandraBrRo <alrobrand@gmail.com>
2023-01-28 23:27:26 +0200

    not gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ronn/dicourse',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '457cc0bfdbbd18b8da204aadb36f91cf1e781b95',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ronn/dicourse' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '457cc0bfdbbd18b8da204aadb36f91cf1e781b95',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
